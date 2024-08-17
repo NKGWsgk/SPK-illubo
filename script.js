2c8556dda2065a29b94079d1f6097d5f976ef13f
NKGWsgk <shigeki1046@gmail.com>
2024-08-17 20:19:47 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function playJapanese() {
-    var utterance = new SpeechSynthesisUtterance("これはテストのための読み上げサンプルです。");
+    var utterance = new SpeechSynthesisUtterance("郵便局");
     utterance.lang = 'ja-JP';
     window.speechSynthesis.speak(utterance);
 }
 
 function playEnglish() {
-    var utterance = new SpeechSynthesisUtterance("This is a sample text for testing the speech synthesis.");
+    var utterance = new SpeechSynthesisUtterance("Post office");
     utterance.lang = 'en-US';
     window.speechSynthesis.speak(utterance);
 }
