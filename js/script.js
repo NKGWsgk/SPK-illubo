3fa53225fc4bc3888471421031af64a40c401898
NKGWsgk <shigeki1046@gmail.com>
2024-08-25 17:05:32 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,9 +2,3 @@ function playJapanese() {
     var audio = new Audio('https://github.com/NKGWsgk/SPK-illubo/raw/main/audio/0-SPK-jiten.wav');
     audio.play();
 }
-
-function playEnglish() {
-    var utterance = new SpeechSynthesisUtterance("Post office");
-    utterance.lang = 'en-US';
-    window.speechSynthesis.speak(utterance);
-}
