0941f46b8939cd4b76f4ba6e746225ab73c0e9eb
NKGWsgk <shigeki1046@gmail.com>
2024-08-25 17:20:04 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
-function playJapanese() {
-    var audio = new Audio('https://github.com/NKGWsgk/SPK-illubo/raw/main/audio/0-SPK-jiten.wav');
-    audio.play();
+var currentLanguage = 'ja'; // デフォルトは日本語
+
+function playAudio() {
+    if (currentLanguage === 'ja') {
+        var audio = new Audio('https://github.com/NKGWsgk/SPK-illubo/raw/main/audio/0-SPK-jiten.wav');
+        audio.play();
+    } else if (currentLanguage === 'en') {
+        var utterance = new SpeechSynthesisUtterance("Post office");
+        utterance.lang = 'en-US';
+        window.speechSynthesis.speak(utterance);
+    }
+}
+
+function toggleLanguage() {
+    var button = document.getElementById('langButton');
+    if (currentLanguage === 'ja') {
+        currentLanguage = 'en';
+        button.textContent = 'English';
+    } else {
+        currentLanguage = 'ja';
+        button.textContent = '日本語';
+    }
 }
